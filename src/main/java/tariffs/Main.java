e9ba0afa9c13234e4fa80452fc2e36989e35f59d
Mariia Sluka <sluka.maria@gmail.com>
2019-10-28 22:12:01 +0200

    init operator map

--- src/main/java/tariffs/Main.java
+++ src/main/java/tariffs/Main.java
@@ -1,10 +1,30 @@
 package main.java.tariffs;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
 
     }
+    public static final String TARIFF_KYIVSTAR = "KYIVSTAR";
+
+    public static final Map<String, List<Tariff>> stringListMap = new HashMap<>();
+
+    static {
+        List<Tariff> tariffsKyivstar = new ArrayList<>();
+        Tariff tariff_1 = new Tariff();
+        tariffsKyivstar.add(tariff_1);
+
+        Tariff tariff_2 = new Tariff();
+        tariffsKyivstar.add(tariff_2);
+
+        stringListMap.put(TARIFF_KYIVSTAR, tariffsKyivstar);
+    }
+
 }
