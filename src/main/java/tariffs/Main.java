788c46baa39fabb30770b56dbac9f450121f8e06
Mariia Sluka <sluka.maria@gmail.com>
2019-10-29 10:07:37 +0200

    add tariffs Lifecell

--- src/main/java/tariffs/Main.java
+++ src/main/java/tariffs/Main.java
@@ -13,18 +13,27 @@ public class Main {
 
     }
     public static final String OPERATOR_KYIVSTAR = "KYIVSTAR";
+    public static final String OPERATOR_LIFECELL = "LIFECELL";
 
     public static final Map<String, List<Tariff>> stringListMap = new HashMap<>();
 
     static {
         List<Tariff> tariffsKyivstar = new ArrayList<>();
+        List<Tariff> tariffsLifecell = new ArrayList<>();
         Tariff tariff_1 = new Tariff("Super", 12,32,43,43,1);
         tariffsKyivstar.add(tariff_1);
 
         Tariff tariff_2 = new Tariff("Turbo", 54,24,12,4,100);
-        tariffsKyivstar.add(tariff_2);
+        boolean add = tariffsKyivstar.add(tariff_2);
+
+        Tariff tariff_3 = new Tariff("Max",43, 42, 656, 53,23);
+        tariffsLifecell.add(tariff_3);
+
+        Tariff tariff_4 = new Tariff("Max",43, 42, 656, 53,23);
+        tariffsLifecell.add(tariff_4);
 
         stringListMap.put(OPERATOR_KYIVSTAR, tariffsKyivstar);
+        stringListMap.put(OPERATOR_LIFECELL, tariffsLifecell);
     }
 
 }
