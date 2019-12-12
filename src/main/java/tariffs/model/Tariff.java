0e29e4918952c552babd57822c470573f20c181f
Mariia Sluka <sluka.maria@gmail.com>
2019-12-13 01:09:19 +0200

    edit Tariff: variables are made by constants

--- src/main/java/tariffs/model/Tariff.java
+++ src/main/java/tariffs/model/Tariff.java
@@ -2,13 +2,21 @@ package tariffs.model;
 
 import nu.xom.Element;
 import nu.xom.Elements;
-import tariffs.model.XMLSerializable;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Tariff implements Comparable<Tariff>, XMLSerializable {
 
+    public static final String NAME = "name";
+    public static final String PRICE = "price";
+    public static final String INTERNET = "internet";
+    public static final String CALL = "call";
+    public static final String SMS = "sms";
+    public static final String CALL_TO_ANOTHER = "callToAnother";
+    public static final String ROAMING = "roaming";
+    public static final String CLIENTS = "clients";
+    public static final String NUMBER = "number";
     private String name;
     private Integer price;
     private Integer internet;
@@ -32,17 +40,17 @@ public class Tariff implements Comparable<Tariff>, XMLSerializable {
 
     public Tariff(Element elTariff) {
 
-        this.name = elTariff.getFirstChildElement("name").getValue();
-        this.price = Integer.parseInt(elTariff.getFirstChildElement("price").getValue());
-        this.internet = Integer.parseInt(elTariff.getFirstChildElement("internet").getValue());
-        this.call = Integer.parseInt(elTariff.getFirstChildElement("call").getValue());
-        this.sms = Integer.parseInt(elTariff.getFirstChildElement("sms").getValue());
-        this.callsToAnother = Integer.parseInt(elTariff.getFirstChildElement("callToAnother").getValue());
-        this.roaming = Integer.parseInt(elTariff.getFirstChildElement("roaming").getValue());
+        this.name = elTariff.getFirstChildElement(NAME).getValue();
+        this.price = Integer.parseInt(elTariff.getFirstChildElement(PRICE).getValue());
+        this.internet = Integer.parseInt(elTariff.getFirstChildElement(INTERNET).getValue());
+        this.call = Integer.parseInt(elTariff.getFirstChildElement(CALL).getValue());
+        this.sms = Integer.parseInt(elTariff.getFirstChildElement(SMS).getValue());
+        this.callsToAnother = Integer.parseInt(elTariff.getFirstChildElement(CALL_TO_ANOTHER).getValue());
+        this.roaming = Integer.parseInt(elTariff.getFirstChildElement(ROAMING).getValue());
 
         this.clients = new LinkedList<Client>();
-        Element elClients = elTariff.getFirstChildElement("clients");
-        Elements elsClients = elClients.getChildElements("number");
+        Element elClients = elTariff.getFirstChildElement(CLIENTS);
+        Elements elsClients = elClients.getChildElements(NUMBER);
 
         for(int i = 0, n = elsClients.size(); i < n; i++) {
             this.clients.add(new Client(elsClients.get(i)));
@@ -110,6 +118,10 @@ public class Tariff implements Comparable<Tariff>, XMLSerializable {
         return clients;
     }
 
+    public Integer getSizeOfClients() {
+        return clients.size();
+    }
+
     public void setClients(List<Client> clients) {
         this.clients = clients;
     }
@@ -118,15 +130,15 @@ public class Tariff implements Comparable<Tariff>, XMLSerializable {
 
         Element tariff = new Element("tariff");
 
-        Element elName = new Element("name");
-        Element elPrice = new Element("price");
-        Element elInternet = new Element("internet");
-        Element elCall = new Element("call");
+        Element elName = new Element(NAME);
+        Element elPrice = new Element(PRICE);
+        Element elInternet = new Element(INTERNET);
+        Element elCall = new Element(CALL);
         Element elSms = new Element("sms");
-        Element elCallToAnother = new Element("callToAnother");
-        Element elRoaming = new Element("roaming");
+        Element elCallToAnother = new Element(CALL_TO_ANOTHER);
+        Element elRoaming = new Element(ROAMING);
 
-        Element elClients = new Element("clients");
+        Element elClients = new Element(CLIENTS);
         for (Client cl: clients) {
             elClients.appendChild(cl.getXML());
         }
