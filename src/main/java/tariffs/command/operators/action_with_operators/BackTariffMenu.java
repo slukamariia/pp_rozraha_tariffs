25391ee4079f90ebbd5405214007fd188377142e
Mariia Sluka <sluka.maria@gmail.com>
2019-12-12 23:44:58 +0200

    added (back tariff menu)

---
+++ src/main/java/tariffs/command/operators/action_with_operators/BackTariffMenu.java
@@ -0,0 +1,10 @@
+package tariffs.command.operators.action_with_operators;
+
+import tariffs.command.Command;
+
+public class BackTariffMenu implements Command {
+    @Override
+    public void execute() {
+        System.out.println("Exit form action with operator menu");
+    }
+}
