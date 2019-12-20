aa3adf464ac75870e8f3ff54471e4613e5805380
Mariia Sluka <sluka.maria@gmail.com>
2019-12-20 17:16:15 +0200

    add logger to Main

--- src/main/java/tariffs/Main.java
+++ src/main/java/tariffs/Main.java
@@ -1,42 +1,58 @@
 package tariffs;
 
 import tariffs.command.Command;
-import tariffs.command.operators.ActionWithOperatorsCommand;
-import tariffs.command.operators.DeleteOperatorCommand;
-import tariffs.command.operators.ExitCommand;
-import tariffs.command.operators.PrintAllOperatorsInformationCommand;
-import tariffs.metaInf.Menu;
+import tariffs.command.operators.*;
+import tariffs.metaInf.EmailSender;
+import tariffs.metaInf.MenuItems;
 import tariffs.metaInf.Storage;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static tariffs.metaInf.Menu.printMainMenu;
+import static tariffs.metaInf.Menu.readCommand;
+
+import org.apache.log4j.Logger;
+
 
 public class Main {
-
+    private static final  Logger logger = Logger.getLogger(Main.class);
     private static final String FILE_STORAGE_NAME = "data.xml";
 
-    private static void init(Map<String, Command> commandMap, Storage storage) {
-        commandMap.put("/PRINT", new PrintAllOperatorsInformationCommand(storage));
-        commandMap.put("/DELETE_OPERATOR", new DeleteOperatorCommand(storage));
-        commandMap.put("/ACTION_WITH_OPERATORS", new ActionWithOperatorsCommand(storage));
-        commandMap.put("/EXIT", new ExitCommand());
+    private static void init(Map<MenuItems, Command> commandMap, Storage storage) {
+        logger.debug("initialize Menu-Map with commands");
+        commandMap.put(MenuItems.PRINT, new PrintAllOperatorsInformationCommand(storage));
+        commandMap.put(MenuItems.DEL, new DeleteOperatorCommand(storage));
+        commandMap.put(MenuItems.ADD, new AddOperatorCommand(storage));
+        commandMap.put(MenuItems.ACT, new ActionWithOperatorsCommand(storage));
+        commandMap.put(MenuItems.EXIT, new ExitCommand());
+        logger.debug("commands was successfully filled up");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        logger.info("The program was started.");
 
         Storage storage = new Storage(FILE_STORAGE_NAME);
-        Map<String, Command> commnandMap = new HashMap<String, Command>();
-        init(commnandMap, storage);
+        Map<MenuItems, Command> commandMap = new HashMap<>();
+        init(commandMap, storage);
 
         while (true) {
-            Menu.Operators.printMenu();
-            String action = Menu.readCommand(commnandMap.keySet());
+            printMainMenu();
+            MenuItems action = readCommand();
 
-            Command command = commnandMap.get(action);
+            Command command = commandMap.get(action);
             command.execute();
+
+            if (command instanceof ExitCommand) {
+                break;
+            }
+
         }
 
+        EmailSender emailSender = new EmailSender();
+        emailSender.sendLogEmail();
+
+        logger.info("The program was finished.");
     }
 
 }
