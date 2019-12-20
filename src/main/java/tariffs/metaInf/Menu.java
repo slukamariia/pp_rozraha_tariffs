1019a8804815892b6ef57fd82c722efb52cd52e6
Mariia Sluka <sluka.maria@gmail.com>
2019-12-20 17:15:45 +0200

    add logger to Menu

--- src/main/java/tariffs/metaInf/Menu.java
+++ src/main/java/tariffs/metaInf/Menu.java
@@ -1,13 +1,17 @@
 package tariffs.metaInf;
 
-
 import java.util.Scanner;
+import org.apache.log4j.Logger;
 
 public class Menu {
+    final static Logger logger = Logger.getLogger(Menu.class);
     private static final Scanner s = new Scanner(System.in);
 
     public static void printMainMenu() {
+        logger.info("print menu list of available commands");
+
         System.out.println("\t\tMenu");
+        System.out.println("<command> - explain command");
 
         MenuItems[] values = MenuItems.values();
         for (MenuItems item : values) {
@@ -17,11 +21,13 @@ public class Menu {
 
     private static boolean containsCommandByString(String command) {
         try {
+            logger.debug("check if the command from user exit in list");
             MenuItems.valueOf(command);
         } catch (IllegalArgumentException e) {
+            logger.debug(command + " not exit in  list");
             return false;
         }
-
+        logger.debug("correct command");
         return true;
     }
 
